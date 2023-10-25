a192e84d52e51eac6d3441ddc8471cf9f2bbfdb4
Ujjwal6371 <94676604+Ujjwal6371@users.noreply.github.com>
2023-10-25 15:45:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function WordCounter() {
+  const [paragraph, setParagraph] = useState('');
+  const wordCount = paragraph.split(/\s+/).filter(word => word !== '').length;
+
+  const handleParagraphChange = (e) => {
+    setParagraph(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="word-counter-container">
+      <h1>Responsive Paragraph Word Counter</h1>
+      <div className="paragraph-box">
+        <textarea
+          rows="5"
+          placeholder="Enter your paragraph here..."
+          value={paragraph}
+          onChange={handleParagraphChange}
+        />
+      </div>
+      <p>Word Count: {wordCount}</p>
     </div>
   );
 }
 
-export default App;
+export default WordCounter;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
